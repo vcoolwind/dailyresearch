8465e358c13a7223188ff3d0cd3bfd63e398e240
王彦锋 <wangyf@jjmmw.com>
2018-04-16 21:53:39 +0800

    add comment

--- src/main/java/com/balckstone/dailyresearch/bigdata/wordcountsort/WordCountSortMain.java
+++ src/main/java/com/balckstone/dailyresearch/bigdata/wordcountsort/WordCountSortMain.java
@@ -44,6 +44,10 @@ public class WordCountSortMain implements Runner {
             job.setReducerClass(WordCountReducer.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(IntWritable.class);
+            //OutPutFormat类，该类负责输出结果的格式。可以通过job.setOutputFormatClass()方法进行设置。
+            // 默认使用TextOUtputFormat类，得到<Key,value>对。
+
+            //指定输出SequenceFile，减少碎文件。
             job.setOutputFormatClass(SequenceFileOutputFormat.class);
             job.waitForCompletion(true);
 
@@ -53,6 +57,9 @@ public class WordCountSortMain implements Runner {
             FileInputFormat.addInputPath(sortJob, tempDir);
             FileOutputFormat.setOutputPath(sortJob, new Path(args[1]));
 
+            //TextInputFormat<LongWritable,Text>这个是默认的数据格式类。
+            // key代表当前行数据距离文件开始的距离，value代码当前行字符串。
+            //这里使用SequenceFile，是利用在上面输出的是SequenceFile。
             sortJob.setInputFormatClass(SequenceFileInputFormat.class);
             sortJob.setMapperClass(InverseMapper.class);
             sortJob.setNumReduceTasks(1);
