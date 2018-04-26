6a2021b39b725eb56ac20ad82f25d72cf4a67382
王彦锋 <wangyf@jjmmw.com>
2018-04-26 20:40:05 +0800

    config test

---
+++ src/main/java/com/balckstone/dailyresearch/bigdata/config/ConfigurationTest.java
@@ -0,0 +1,24 @@
+package com.balckstone.dailyresearch.bigdata.config;
+
+
+import org.apache.hadoop.conf.Configuration;
+
+public class ConfigurationTest {
+    public static void main(String[] args) {
+        Configuration configuration =new Configuration();
+        configuration.addResource("configuration-1.xml");
+
+        System.out.println("yellow".equals(configuration.get("color")));
+        System.out.println("16".equals(configuration.get("size")));
+        System.out.println(configuration.get("color-size"));
+
+        //配置文件会被覆盖，但final的属性不会。
+        configuration.addResource("configuration-2.xml");
+        System.out.println("red".equals(configuration.get("color")));
+        System.out.println("16".equals(configuration.get("size")));
+        System.out.println("1024".equals(configuration.get("width")));
+
+        System.out.println(configuration.get("color-size"));
+
+    }
+}
